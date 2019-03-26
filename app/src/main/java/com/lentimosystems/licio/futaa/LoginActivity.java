82cfcaffa36164356bb3735318ee4a1d69dd00b5
liciolentimo <lentimo@gmail.com>
2019-03-26 13:23:42 +0300

    Added splash screen

---
+++ app/src/main/java/com/lentimosystems/licio/futaa/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.lentimosystems.licio.futaa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
+import com.flaviofaria.kenburnsview.Transition;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
--- app/src/main/java/com/lentimosystems/licio/futaa/MainActivity.java
+++ app/src/main/java/com/lentimosystems/licio/futaa/MainActivity.java
@@ -1,13 +1,59 @@
 package com.lentimosystems.licio.futaa;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Toast;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
+import com.flaviofaria.kenburnsview.Transition;
 
 public class MainActivity extends AppCompatActivity {
+    private KenBurnsView kenburns;
+    private boolean moving = true;
+    KenBurnsView mKenburns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kenburns = findViewById(R.id.kenburns);
+
+        Typeface openSansFont = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Regular.ttf");
+
+        AccelerateDecelerateInterpolator adi = new AccelerateDecelerateInterpolator();
+        RandomTransitionGenerator generator = new RandomTransitionGenerator(3000, adi);
+        kenburns.setTransitionGenerator(generator);
+
+        kenburns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (moving) {
+                    kenburns.pause();
+                    moving = false;
+                } else {
+                    kenburns.resume();
+                    moving = true;
+                }
+            }
+        });
+
+        kenburns.setTransitionListener(new KenBurnsView.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
